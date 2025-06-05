f8a5c0569c02f8c95bcd06fec9e0ca007eb2274b
Nasusann <kawarayako@cy-com.co.jp>
2025-06-05 13:45:41 +0900

    修正

--- src/test/java/com/example/music_management/MusicManagementApplicationTests.java
+++ src/test/java/com/example/music_management/MusicManagementApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.music_management;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.musicmanagement.MusicManagementApplication;
+
+@SpringBootTest(classes = MusicManagementApplication.class)
 class MusicManagementApplicationTests {
 
 	@Test
--- src/test/java/com/sample/music_management/MusicManagementApplicationTests.java
+++ src/test/java/com/sample/music_management/MusicManagementApplicationTests.java
@@ -3,7 +3,9 @@ package com.sample.music_management;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.musicmanagement.MusicManagementApplication;
+
+@SpringBootTest(classes = MusicManagementApplication.class)
 class MusicManagementApplicationTests {
 
 	@Test
