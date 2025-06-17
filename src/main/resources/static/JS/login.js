3a451ed388f6c990585b51da04c10c06a201bc24
Nasusann <kawarayako@cy-com.co.jp>
2025-06-17 17:14:10 +0900

    音削除

--- src/main/resources/static/JS/login.js
+++ src/main/resources/static/JS/login.js
@@ -6,8 +6,3 @@ document.addEventListener('mousemove', (event) => {
   circle.style.left = (event.pageX - offsetX) + 'px';
   circle.style.top = (event.pageY - offsetY) + 'px';
 });
-
-const audio = document.getElementById('audio');
-document.getElementById('play-sound').addEventListener('click', () => { audio.play();});
-
-audio.addEventListener('ended', () => { alert('再生完了しました'); });
