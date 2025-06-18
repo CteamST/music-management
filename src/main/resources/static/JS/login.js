782d31909feb265299cb26a20336924f8902e2b6
Sasakitsubasa3 <sasakitsu@cy-com.co.jp>
2025-06-18 09:39:44 +0900

    文字が浮き出てきます！

--- src/main/resources/static/JS/login.js
+++ src/main/resources/static/JS/login.js
@@ -6,3 +6,16 @@ document.addEventListener('mousemove', (event) => {
   circle.style.left = (event.pageX - offsetX) + 'px';
   circle.style.top = (event.pageY - offsetY) + 'px';
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const title = document.querySelector('.slide-bounce-title');
+  const text = "わんわーく";
+  title.innerHTML = ''; // 初期化
+
+  [...text].forEach((char, i) => {
+    const span = document.createElement('span');
+    span.textContent = char;
+    span.style.animationDelay = `${i * 0.15}s`;
+    title.appendChild(span);
+  });
+});
